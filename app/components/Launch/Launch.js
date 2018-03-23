268a1cbe770479c938c80bdd4a3020c79ed9c5a4
Amal <raj.amal@gmail.com>
2018-03-23 15:45:05 +0530

    Initial bots version 2.5.0(3)

--- app/components/Launch/Launch.js
+++ app/components/Launch/Launch.js
@@ -18,7 +18,7 @@ import { DeviceStorage } from '../../lib/capability';
 import { ContactsCache } from '../../lib/ContactsCache';
 import { MessageCounter } from '../../lib/MessageCounter';
 
-const VERSION = 8; // Corresponding to 2.5.0 build 1. Update this number every time we update initial_bots
+const VERSION = 9; // Corresponding to 2.5.0 build 3. Update this number every time we update initial_bots
 const VERSION_KEY = 'version';
 
 export default class Splash extends React.Component {
