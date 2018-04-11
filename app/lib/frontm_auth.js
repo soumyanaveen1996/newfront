1c1883d0a3b1660972b09eed0cba658a97ec9a58
Amal <raj.amal@gmail.com>
2018-04-11 16:02:09 +0530

    Fix refresh call

--- app/lib/frontm_auth.js
+++ app/lib/frontm_auth.js
@@ -290,7 +290,7 @@ class FrontmAuth {
             'url': Config.proxy.protocol + Config.proxy.host + Config.proxy.refreshPath,
             'headers': {
                 refresh_token: user.provider.refreshToken,
-                provider_name: _.lowerCase(user.provider.name),
+                provider_name: user.provider.name.toLowerCase(),
                 email: user.info.emailAddress
             }
         };
