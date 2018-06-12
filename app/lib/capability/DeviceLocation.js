92ca8fb7c644a738e6783ee689b54a4c33bd6d68
Amal <raj.amal@gmail.com>
2018-06-12 23:12:32 +0530

    Fix for FP-677

--- app/lib/capability/DeviceLocation.js
+++ app/lib/capability/DeviceLocation.js
@@ -1,3 +1,25 @@
+import Permissions from 'react-native-permissions';
+
+export class LocationError extends Error {
+    constructor(code, message) {
+        super();
+        this.code = code;
+        this.message = message;
+    }
+
+    get code() {
+        return this.code;
+    }
+
+    get message() {
+        return this.message;
+    }
+}
+
+export const LocationErrorCodes = {
+    0: 'Error in getting location',
+    1: 'User rejected location permissions',
+}
 
 export default class DeviceLocation {
 
@@ -6,16 +28,23 @@ export default class DeviceLocation {
      * Returns a promise that resolves to { latitude: 'latitude',longitude: 'longitude' } on success
     */
     static getDeviceLocation = () => new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((location) => {
-            console.log('Location : ', JSON.stringify(location));
-            let deviceLocation = {
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude
-            };
-            resolve(deviceLocation);
-        }, (error) => {
-            console.log('error in geolocation : ', error);
-            reject(error);
-        });
+        Permissions.request('location')
+            .then((response) => {
+                if (response === 'authorized') {
+                    navigator.geolocation.getCurrentPosition((location) => {
+                        console.log('Location : ', JSON.stringify(location));
+                        let deviceLocation = {
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude
+                        };
+                        resolve(deviceLocation);
+                    }, (error) => {
+                        reject(new LocationErrorCodes(0, LocationErrorCodes[0]));
+                    });
+                } else {
+                    reject(new LocationErrorCodes(1, LocationErrorCodes[1]));
+                }
+            });
+
     });
 }
