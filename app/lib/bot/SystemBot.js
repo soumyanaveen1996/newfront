a0c9ab9cd75924a6f8eeda19c9ae00a4efe3c7e3
Rashmi Kamath <rush@frontm.com>
2018-06-15 09:52:17 +0100

    version changes for onboarding bot

--- app/lib/bot/SystemBot.js
+++ app/lib/bot/SystemBot.js
@@ -50,7 +50,7 @@ export const SYSTEM_BOT_MANIFEST = {
         'allowResetConversation': 'false',
         'botId': 'onboarding-bot',
         'botName': 'M',
-        'botUrl': 'botfarm/frontmai/onboardingBot/1.0.0/onboardingBot.js',
+        'botUrl': 'botfarm/frontmai/onboardingBot/1.1.0/onboardingBot.js',
         'category': [],
         'dependencies': {
             'agentGuardService': {
@@ -60,8 +60,8 @@ export const SYSTEM_BOT_MANIFEST = {
             },
             'authContext': {
                 'remote': 'true',
-                'url': 'botfarm/rc/frontmai/authContext/1.0.0/authContext.js',
-                'version': '1.0.0'
+                'url': 'botfarm/rc/frontmai/authContext/1.1.0/authContext.js',
+                'version': '1.1.0'
             }
         },
         'description': 'Onboarding bot for Frontm',
@@ -69,7 +69,7 @@ export const SYSTEM_BOT_MANIFEST = {
         'slug': 'onboarding-bot',
         'systemBot': true,
         'userDomain': 'frontmai',
-        'version': '1.0.0'
+        'version': '1.1.0'
     },
     'contacts-bot': {
         'allowResetConversation': 'false',
