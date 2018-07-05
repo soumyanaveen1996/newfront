52c6dd7e5d25e8897ecb948cfb38243fd22ef8fa
Amal <raj.amal@gmail.com>
2018-07-05 20:00:13 +0530

    New State capability

--- app/lib/capability/index.js
+++ app/lib/capability/index.js
@@ -17,6 +17,7 @@ import Resource, { ResourceTypes } from './Resource';
 import { AUTH_PROVIDERS } from './Auth';
 import DeviceLocation from './DeviceLocation';
 import Telnet from './Telnet';
+import State from './State';
 
 const version = '1.0'; // Keeps getting bumped if new capabilities are added (after release 1)
 
@@ -45,5 +46,6 @@ export {
     PollingStrategyTypes,
     version,
     DeviceLocation,
-    Telnet
+    Telnet,
+    State,
 };
