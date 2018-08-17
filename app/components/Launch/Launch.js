db31b5a885f1f90226aacfc3aa54e72aaa479d68
Amal <raj.amal@gmail.com>
2018-08-17 18:24:19 +0530

    v 2.14.0(4)(18)

--- app/components/Launch/Launch.js
+++ app/components/Launch/Launch.js
@@ -21,7 +21,7 @@ import { Telnet } from '../../lib/capability';
 import SystemBot from '../../lib/bot/SystemBot';
 import { BackgroundBotChat } from '../../lib/BackgroundTask';
 
-const VERSION = 29; // Corresponding to 2.13.1 build 1. Update this number every time we update initial_bots
+const VERSION = 30; // Corresponding to 2.14.0 build 4. Update this number every time we update initial_bots
 const VERSION_KEY = 'version';
 
 export default class Splash extends React.Component {
@@ -52,7 +52,7 @@ export default class Splash extends React.Component {
 
         if (forceUpdate) {
             console.log('Copying Bots');
-            //await BotUtils.copyIntialBots(forceUpdate);
+            await BotUtils.copyIntialBots(forceUpdate);
             await DeviceStorage.save(VERSION_KEY, VERSION);
         }
 
