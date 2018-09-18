b5a1467191b99fc319f97af3afd4dbc418f55332
Rashmi Kamath <rush@frontm.com>
2018-09-18 13:29:17 +0100

    System bot changes

--- app/lib/bot/SystemBot.js
+++ app/lib/bot/SystemBot.js
@@ -53,7 +53,7 @@ export const SYSTEM_BOT_MANIFEST = {
         'allowResetConversation': 'false',
         'botId': 'onboarding-bot',
         'botName': 'FrontM Assistant',
-        'botUrl': 'botfarm/frontmai/onboardingBot/1.6.0/onboardingBot.js',
+        'botUrl': 'botfarm/frontmai/onboardingBot/1.7.0/onboardingBot.js',
         'category': [],
         'dependencies': {
             'agentGuardService': {
@@ -63,8 +63,8 @@ export const SYSTEM_BOT_MANIFEST = {
             },
             'authContext': {
                 'remote': 'true',
-                'url': 'botfarm/rc/frontmai/authContext/1.3.0/authContext.js',
-                'version': '1.3.0'
+                'url': 'botfarm/rc/frontmai/authContext/1.4.0/authContext.js',
+                'version': '1.4.0'
             }
         },
         'description': 'Onboarding bot for Frontm',
@@ -72,7 +72,7 @@ export const SYSTEM_BOT_MANIFEST = {
         'slug': 'onboarding-bot',
         'systemBot': true,
         'userDomain': 'frontmai',
-        'version': '1.6.0'
+        'version': '1.7.0'
     },
     'contacts-bot': {
         'allowResetConversation': 'false',
@@ -108,7 +108,7 @@ export const SYSTEM_BOT_MANIFEST = {
         'allowResetConversation': 'false',
         'botId': 'de3a7fe6-b2a7-4468-8099-40c57848387d',
         'botName': 'Channels',
-        'botUrl': 'botfarm/frontmai/channelsBot/1.3.0/channelsBot.js',
+        'botUrl': 'botfarm/frontmai/channelsBot/1.4.0/channelsBot.js',
         'category': [],
         'dependencies': {
             'agentGuardService': {
@@ -128,13 +128,13 @@ export const SYSTEM_BOT_MANIFEST = {
         'slug': 'channels-bot',
         'systemBot': true,
         'userDomain': 'frontmai',
-        'version': '1.3.0'
+        'version': '1.4.0'
     },
     'domMgmt-bot': {
         'allowResetConversation': 'false',
         'botId': 'domMgmtBot',
         'botName': 'Sign in to a new Provider',
-        'botUrl': 'botfarm/frontmai/domMgmtBot/1.1.0/domMgmtBot.js',
+        'botUrl': 'botfarm/frontmai/domMgmtBot/1.2.0/domMgmtBot.js',
         'category': [],
         'dependencies': {
             'agentGuardService': {
@@ -144,8 +144,8 @@ export const SYSTEM_BOT_MANIFEST = {
             },
             'authContext': {
                 'remote': true,
-                'url': 'botfarm/rc/frontmai/authContext/1.0.0/authContext.js',
-                'version': '1.0.0'
+                'url': 'botfarm/rc/frontmai/authContext/1.4.0/authContext.js',
+                'version': '1.4.0'
             }
         },
         'description': 'Onboarding bot for Frontm',
@@ -153,7 +153,7 @@ export const SYSTEM_BOT_MANIFEST = {
         'slug': 'domMgmt-bot',
         'systemBot': true,
         'userDomain': 'frontmai',
-        'version': '1.1.0'
+        'version': '1.2.0'
     },
     'backgroundTask-bot': {
         'allowResetConversation': 'false',
