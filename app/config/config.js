f2cc634db05bcaacec7d447a0f3978237ae362e4
Sourav Chatterjee <gooner.sourav@gmail.com>
2018-09-19 15:26:43 +0530

    Reveretd Changes to point back to dev server

--- app/config/config.js
+++ app/config/config.js
@@ -1,32 +1,32 @@
-// import configToUse from './env/dev';
-import configToUse from "./env/local";
+import configToUse from './env/dev';
+// import configToUse from "./env/local";
 
 // Point to the right environment config based on what is being released
 const config = configToUse;
 
 export const overrideConsole = trueConsole => {
-  return {
-    log: function() {
-      if (global.__DEV__) {
-        trueConsole.log.apply(trueConsole, arguments);
-      }
-    },
-    error: function() {
-      if (global.__DEV__) {
-        trueConsole.log.apply(trueConsole, arguments);
-      }
-    },
-    warn: function() {
-      if (global.__DEV__) {
-        trueConsole.log.apply(trueConsole, arguments);
-      }
-    },
-    info: function() {
-      if (global.__DEV__) {
-        trueConsole.log.apply(trueConsole, arguments);
-      }
-    }
-  };
+    return {
+        log: function() {
+            if (global.__DEV__) {
+                trueConsole.log.apply(trueConsole, arguments);
+            }
+        },
+        error: function() {
+            if (global.__DEV__) {
+                trueConsole.log.apply(trueConsole, arguments);
+            }
+        },
+        warn: function() {
+            if (global.__DEV__) {
+                trueConsole.log.apply(trueConsole, arguments);
+            }
+        },
+        info: function() {
+            if (global.__DEV__) {
+                trueConsole.log.apply(trueConsole, arguments);
+            }
+        }
+    };
 };
 
 export default config;
