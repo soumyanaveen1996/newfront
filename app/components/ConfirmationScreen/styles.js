00005f15518301606a1e0e56136a4d868a361e1c
Siddharth Hembrom <sid@frontm.com>
2018-11-01 23:55:27 +0530

    1191 fixed new Signup confirmation screen

--- app/components/ConfirmationScreen/styles.js
+++ app/components/ConfirmationScreen/styles.js
@@ -12,7 +12,7 @@ export default StyleSheet.create({
     innerContainer: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: 'rgba(0,0,0,0)'
+        backgroundColor: 'rgba(255,255,255,1)'
     },
     header: {
         width: 300,
@@ -68,10 +68,17 @@ export default StyleSheet.create({
         fontWeight: 'normal',
         fontSize: 16
     },
+    pinCodeWithPassword: {
+        flex: 1,
+        alignItems: 'center',
+        marginBottom: 5,
+        marginTop: 10,
+        justifyContent: 'center'
+    },
     pinCode: {
         flex: 1,
         alignItems: 'center',
-        paddingBottom: 60,
+        marginBottom: 60,
         marginTop: 10,
         justifyContent: 'center'
     },
@@ -93,7 +100,7 @@ export default StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 40
+        marginTop: 70
     },
     resendButton: {
         flex: 1,
@@ -104,5 +111,46 @@ export default StyleSheet.create({
         color: 'rgba(0, 189, 242, 1)',
         fontSize: 16,
         textAlign: 'center'
+    },
+    entryFields: Platform.select({
+        ios: {
+            position: 'relative',
+            flex: 1,
+            backgroundColor: 'transparent',
+            marginBottom: 40,
+            alignItems: 'center',
+            marginTop: 10,
+            justifyContent: 'center'
+        },
+        android: {
+            flex: 1,
+            backgroundColor: 'transparent',
+            alignItems: 'center',
+            marginBottom: 45,
+            marginTop: 10,
+            justifyContent: 'center'
+        }
+    }),
+    input: {
+        height: 40,
+        width: 300,
+        backgroundColor: 'rgba(244,244,244,1)',
+        padding: 10,
+        color: 'rgba(0,0,0,0.8)',
+        fontSize: 16,
+        borderTopRightRadius: 0,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        borderTopLeftRadius: 10
+    },
+    placeholderText: {
+        fontWeight: '300',
+        color: 'rgba(74,74,74,1)',
+        width: 300,
+        padding: 4,
+        letterSpacing: 1,
+        lineHeight: 20,
+        fontSize: 14,
+        marginTop: 20
     }
 });
