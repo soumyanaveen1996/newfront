e01bbee2a12ea33b82f7eae5f32dd17254f209d3
Davide <davide@users-MacBook-Pro-2.local>
2018-12-19 16:19:39 +0000

    static vessel positions

--- app/images/index.js
+++ app/images/index.js
@@ -77,7 +77,8 @@ const images = {
     //MAP
     map_starting_point: require('./mapbox/starting-point.png'),
     map_arrival_point: require('./mapbox/arrival-point.png'),
-    maps_maritime_icon: require('./mapbox/maps-maritime-icon.png')
+    maps_maritime_icon: require('./mapbox/maps-maritime-icon.png'),
+    current_location_inactive: require('./mapbox/current-location-inactive.png')
 };
 
 export default images;
