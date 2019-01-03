11c466f6852b58510a9caea7f52b2f63d9b40e00
Sourav Chatterjee <gooner.sourav@gmail.com>
2019-01-03 12:49:11 +0530

    Build

--- android/app/src/main/java/com/frontm/frontm/MainApplication.java
+++ android/app/src/main/java/com/frontm/frontm/MainApplication.java
@@ -8,6 +8,7 @@ import android.media.FaceDetector;
 import android.os.Build;
 
 import com.facebook.react.ReactApplication;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.bugsnag.BugsnagReactNative;
 import com.actionsheet.ActionSheetPackage;
 import com.kevinresol.react_native_default_preference.RNDefaultPreferencePackage;
@@ -74,6 +75,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new RCTMGLPackage(),
             BugsnagReactNative.getPackage(),
             new RNDefaultPreferencePackage(), new ReactVideoPackage(), new RNVersionCheckPackage(),
           new VectorIconsPackage(), new TwilioVoicePackage(false), // <---- pass false to
