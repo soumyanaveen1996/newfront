d5b9d76e34848f5f87c76a9e8bf20b31b39ffbc7
Sourav Chatterjee <gooner.sourav@gmail.com>
2019-03-01 22:48:50 +0530

    Prod Fixes

--- app/components/Launch/Launch.js
+++ app/components/Launch/Launch.js
@@ -48,7 +48,7 @@ import ReduxStore from '../../redux/store/configureStore';
 // Switch off During FINAL PROD RELEASE
 // const CODE_PUSH_ACTIVATE = true;
 const CODE_PUSH_ACTIVATE = false;
-const VERSION = 62; // Corresponding to 2.17.0 build 2. Update this number every time we update initial_bots
+const VERSION = 63; // Corresponding to 2.17.0 build 2. Update this number every time we update initial_bots
 const VERSION_KEY = 'version';
 
 export default class Splash extends React.Component {
