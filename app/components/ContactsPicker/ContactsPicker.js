c1b891ae1397a7f326f8b007f71fefebebcbc427
Sourav Chatterjee <gooner.sourav@gmail.com>
2019-03-28 08:51:39 +0530

    Performance Fix for Lists

--- app/components/ContactsPicker/ContactsPicker.js
+++ app/components/ContactsPicker/ContactsPicker.js
@@ -594,6 +594,12 @@ class ContactsPicker extends React.Component {
                         keyExtractor={(item, index) => item.id}
                         ListHeaderComponent={this.renderButtons}
                         stickySectionHeadersEnabled={false}
+                        // Performance settings
+                        removeClippedSubviews={true} // Unmount components when outside of window
+                        initialNumToRender={2} // Reduce initial render amount
+                        maxToRenderPerBatch={1} // Reduce number in each render batch
+                        maxToRenderPerBatch={100} // Increase time between renders
+                        windowSize={7} // Reduce the window size
                     />
                     {/* <ContactsPickerIndexView
                     onItemPressed={this.onSideIndexItemPressed.bind(this)}
