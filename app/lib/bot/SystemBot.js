06d21cacb0238774c890608be91ce535924e2c7c
Sourav Chatterjee <gooner.sourav@gmail.com>
2019-04-25 12:31:23 +0530

    SystemBot chnages

--- app/lib/bot/SystemBot.js
+++ app/lib/bot/SystemBot.js
@@ -52,7 +52,7 @@ export const SYSTEM_BOT_MANIFEST = {
         allowResetConversation: 'false',
         botId: 'onboarding-bot',
         botName: 'FrontM Assistant',
-        botUrl: 'botfarm/frontmai/onboardingBot/2.2.0/onboardingBot.js',
+        botUrl: 'botfarm/frontmai/onboardingBot/2.2.1/onboardingBot.js',
         category: [],
         dependencies: {
             agentGuardService: {
@@ -78,7 +78,7 @@ export const SYSTEM_BOT_MANIFEST = {
         slug: 'onboarding-bot',
         systemBot: true,
         userDomain: 'frontmai',
-        version: '2.2.0'
+        version: '2.2.1'
     },
     'contacts-bot': {
         allowResetConversation: 'false',
