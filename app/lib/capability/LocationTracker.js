688bf6dcc45ee78d284abd24001b6bbbdc7d7dc7
Sourav Chatterjee <gooner.sourav@gmail.com>
2019-07-30 17:53:18 +0530

    Geo Location

--- app/lib/capability/LocationTracker.js
+++ app/lib/capability/LocationTracker.js
@@ -2,100 +2,124 @@ import RemoteLogger from '../utils/remoteDebugger';
 import BackgroundGeolocation from 'react-native-background-geolocation';
 import moment from 'moment';
 import Device from 'react-native-device-info';
+import DeviceStorage from './DeviceStorage';
 
-export const start_tracking = () => {
-    ////
-    // 1.  Wire up event-listeners
-    //
+export default class LocationTracker {
+    static start_tracking = async data => {
+        ////
+        // 1.  Wire up event-listeners
+        //
 
-    // This handler fires whenever bgGeo receives a location update.
-    BackgroundGeolocation.onLocation(onLocation, onError);
-    // This event fires when the user toggles location-services authorization
-    BackgroundGeolocation.onProviderChange(() => {
-        RemoteLogger(
-            `Provider Has Changed ${moment().format('DD-MM hh:mm:ss')}`
+        await DeviceStorage.save('location_bot', data);
+        // This handler fires whenever bgGeo receives a location update.
+        BackgroundGeolocation.onLocation(
+            LocationTracker.onLocation,
+            LocationTracker.onError
         );
-    });
 
-    ////
-    // 2.  Execute #ready method (required)
-    //
-    BackgroundGeolocation.ready(
-        {
-            reset: true,
-            // Geolocation Config
-            desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
-            distanceFilter: 500,
-            heartbeatInterval: 900,
-            preventSuspend: true,
-            // Activity Recognition
-            stopTimeout: 1,
-            // Application config
-            debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
-            logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
-            stopOnTerminate: false, // <-- Allow the background-service to continue tracking when user closes the app.
-            startOnBoot: true, // <-- Auto start tracking when device is powered-up.
-            disableLocationAuthorizationAlert: false,
-            // HTTP / SQLite config
-            url: 'http://tracker.transistorsoft.com/locations/frontm',
-            params: BackgroundGeolocation.transistorTrackerParams(Device),
-            batchSync: false, // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
-            autoSync: true, // <-- [Default: true]Set true to sync each location to server as it arrives.,,
-            headers: {
-                // <-- Optional HTTP headers
-                // 'X-FOO': 'bar'
-            }
-            // params: {
-            //     // <-- Optional HTTP params
-            //     // auth_token: 'maybe_your_server_authenticates_via_token_YES?'
-            // }
-        },
-        async state => {
-            console.log(
-                '- BackgroundGeolocation is configured and ready: ',
-                state.enabled
+        BackgroundGeolocation.onHeartbeat(LocationTracker.handleHeartBeat);
+
+        // This event fires when the user toggles location-services authorization
+        BackgroundGeolocation.onProviderChange(() => {
+            RemoteLogger(
+                `Provider Has Changed ${moment().format('DD-MM hh:mm:ss')}`
             );
+        });
 
-            if (!state.enabled) {
-                ////
-                // 3. Start tracking!
-                //
-                BackgroundGeolocation.start(function() {
-                    console.log('- Start success');
-                });
+        ////
+        // 2.  Execute #ready method (required)
+        //
+        BackgroundGeolocation.ready(
+            {
+                reset: true,
+                // Geolocation Config
+                desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+                distanceFilter: 10,
+                heartbeatInterval: 180,
+                preventSuspend: true,
+                // Activity Recognition
+                stopTimeout: 1,
+                // Application config
+                debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
+                logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+                stopOnTerminate: false, // <-- Allow the background-service to continue tracking when user closes the app.
+                startOnBoot: true, // <-- Auto start tracking when device is powered-up.
+                disableLocationAuthorizationAlert: false,
+                // HTTP / SQLite config
+                url: 'http://tracker.transistorsoft.com/locations/frontm',
+                params: BackgroundGeolocation.transistorTrackerParams(Device),
+                batchSync: false, // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+                autoSync: true, // <-- [Default: true]Set true to sync each location to server as it arrives.,,
+                headers: {
+                    // <-- Optional HTTP headers
+                    // 'X-FOO': 'bar'
+                }
+                // params: {
+                //     // <-- Optional HTTP params
+                //     // auth_token: 'maybe_your_server_authenticates_via_token_YES?'
+                // }
+            },
+            async state => {
+                console.log(
+                    '- BackgroundGeolocation is configured and ready: ',
+                    state.enabled
+                );
+
+                if (!state.enabled) {
+                    ////
+                    // 3. Start tracking!
+                    //
+                    BackgroundGeolocation.start(function() {
+                        console.log('- Start success');
+                    });
+                }
             }
-        }
-    );
-};
-
-export const stop_tracking = () => {
-    return BackgroundGeolocation.stop();
-};
-
-export const remove_location_listeners = () => {
-    BackgroundGeolocation.removeAllListeners();
-};
-
-const onLocation = async location => {
-    try {
-        const taskId = await BackgroundGeolocation.startBackgroundTask();
-        await RemoteLogger(
-            `Location ${JSON.stringify(location)} --- ${moment().format(
-                'DD-MM hh:mm:ss'
-            )}`
         );
-        BackgroundGeolocation.stopBackgroundTask(taskId);
-    } catch (error) {
-        BackgroundGeolocation.stopBackgroundTask();
-    }
-};
+    };
 
-const onError = async error => {
-    try {
+    static onLocation = async location => {
+        try {
+            const taskId = await BackgroundGeolocation.startBackgroundTask();
+            const data = await DeviceStorage.get('location_bot');
+
+            await RemoteLogger(
+                `Location ${JSON.stringify(location)} --- ${moment().format(
+                    'DD-MM hh:mm:ss'
+                )} with DATA ---> ${JSON.stringify(data)}`
+            );
+            BackgroundGeolocation.stopBackgroundTask(taskId);
+        } catch (error) {
+            BackgroundGeolocation.stopBackgroundTask();
+        }
+    };
+
+    static handleHeartBeat = async event => {
         const taskId = await BackgroundGeolocation.startBackgroundTask();
-        await RemoteLogger(`Error: ${JSON.stringify(error)}`);
+        const data = await DeviceStorage.get('location_bot');
+
+        await RemoteLogger(`Received Heartbeat ${JSON.stringify(event)}`);
+        const location = await BackgroundGeolocation.getCurrentPosition({
+            samples: 1,
+            persist: true
+        });
+        console.log('Sourav Logging:::: Heartbeat Event', location);
         BackgroundGeolocation.stopBackgroundTask(taskId);
-    } catch (error) {
-        BackgroundGeolocation.stopBackgroundTask();
-    }
-};
+    };
+
+    static stop_tracking = () => {
+        return BackgroundGeolocation.stop();
+    };
+
+    static removeAllListeners = () => {
+        BackgroundGeolocation.removeAllListeners();
+    };
+    static onError = async error => {
+        try {
+            const taskId = await BackgroundGeolocation.startBackgroundTask();
+            await RemoteLogger(`Error: ${JSON.stringify(error)}`);
+            BackgroundGeolocation.stopBackgroundTask(taskId);
+        } catch (error) {
+            BackgroundGeolocation.stopBackgroundTask();
+        }
+    };
+}
